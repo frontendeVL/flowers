2c4dfd1c67511f1e3b3e55694aed489bb1cc4e43
Valeriia <alggv.b.2828@gmail.com>
2025-08-03 23:06:07 +0200

    feat: add some effect to flower cards

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import './ProductCard.css'
+import { motion } from 'framer-motion'
 const ProductCard = ({ image, name, price }) => {
   return (
-    <div className="product-card">
+  <motion.div className='product-card'
+  initial={{opacity: 0}}
+  whileInView={{opacity: 1}}
+  transition={{duration: 1}}
+  
+  >
       <img className="product-card-img" src={image} alt={name} />
       <div className="card-content">
         <h3>{name}</h3>
         <p className="priceB">Price: ${price}</p>
         <button className="add-to-cart-btn">Add to cart</button>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
